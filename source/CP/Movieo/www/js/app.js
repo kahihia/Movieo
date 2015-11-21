ef7ab020bba74b0bca5a45d6ba34c26ea794fc24
killthebug <jaipal.singh@research.iiit.ac.in>
2015-11-21 12:01:04 +0530

    adding ngOpenFB messed up

--- source/CP/Movieo/www/js/app.js
+++ source/CP/Movieo/www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('movieo', ['ionic', 'movieo.controllers','movieo.factory',])
+angular.module('movieo', ['ionic', 'movieo.controllers','movieo.factory','ngOpenFB'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
